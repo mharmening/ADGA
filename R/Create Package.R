a07a483dac6c69e04a6896b8d35d801e32018644
mharmening <morten.harmening1997@gmail.com>
2026-05-11 11:55:58 +0200

    Initial commit

---
+++ R/Create Package.R
@@ -0,0 +1,17 @@
+# Git initialisieren
+usethis::use_git()
+
+# Lizenz setzen (MIT ist für akademische Open-Source-Pakete üblich)
+usethis::use_mit_license("Dein Name")
+
+# Roxygen2 für Dokumentation aktivieren
+usethis::use_roxygen_md()
+
+# testthat-Grundgerüst anlegen
+usethis::use_testthat()
+
+# README anlegen
+usethis::use_readme_md()
+
+# News/Changelog (optional, aber gut für Versionierung)
+usethis::use_news_md()
